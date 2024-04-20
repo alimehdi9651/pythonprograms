3136bba0748728c7127df8e0c8f2c4a915d8664b
alimehdi <alimehdi9651@gmail.com>
2024-04-20 16:26:31 +0530

    f

--- practicestring.py
+++ practicestring.py
@@ -3,13 +3,5 @@ str = input("enter your first name : ")
 print(len(str))
  
 # 2.
-#  
-
-str = "mansha" 
-wieght = 83
-if wieght >= 50 and wieght <= 70:
-    print(str, "is elegable for mma")
-else:
-    print("not elegable")
 
 
