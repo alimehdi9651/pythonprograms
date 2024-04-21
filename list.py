092b79e3dcf44603f1f1f63b17b147d485094dc7
alimehdi <alimehdi9651@gmail.com>
2024-04-21 13:11:54 +0530

    basics of list method

---
+++ list.py
@@ -0,0 +1,34 @@
+# student = ["ali", 6.1, 21, "lucknow"]
+
+# print(type(student))
+# student[2] = 22
+# print(student)
+
+
+# # basic methods in lists
+# # 1 
+# student.append(2)
+# print(student)
+list = [5, 2, 1, 4, 3]
+alphalist = ['a', 'c', 'e', 'd']
+# list.sort()
+# print(list)
+# alphalist.sort(reverse=True)
+# print(alphalist)
+
+
+# method .reverse() does not sort the list it just reverse the orignal list
+
+# list.reverse()
+# print(list)
+# list.insert(1,6)
+# print(list)
+
+
+# .pop() function pops the element at perticular position and remove() fuction directly remove the element
+# list.remove(5)
+# print(list)
+list.pop(2)
+print(list)
+
+
--- practicestring.py
+++ practicestring.py
@@ -1,7 +1,29 @@
 # 1.
-str = input("enter your first name : ")
-print(len(str))
+# str = input("enter your first name : ")
+# print(len(str))
  
 # 2.
+# amount = int(input("enter amount : "))
+# if amount
+# a = int(input("enter a number : "))
+# if(a%2 == 0):
+#     print(a,"number is even")
+# else:
+#     print(a,"is odd number")
+# a = int(input("enter first number")) 
+# b = int(input("enter first number")) 
+# c = int(input("enter first number")) 
+# if(a > b and a > c):
+#     print(a, "is gratest")
+# elif(b > a and b > c):
+#     print(b, "is gratest number")
+# elif(c > a and c > b):
+#     print(c, "is gratest number")
 
 
+
+a = int(input("enter a number :"))
+if(a % 7 == 0):
+    print("number is divisible by 7")
+else:
+    print("number is not divisible by seven")
