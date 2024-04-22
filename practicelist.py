16b91333f078f9793a0595e666b695d324879a5b
alimehdi <alimehdi9651@gmail.com>
2024-04-22 12:57:07 +0530

    pratice question of list

---
+++ practicelist.py
@@ -0,0 +1,30 @@
+# movie = []
+# mov = input("enter first movie :")
+# movie.append(mov)
+# mov = input("enter 2nd movie :")
+# movie.append(mov)
+# mov = input("enter 3rd movie name :")
+# movie.append(mov)
+# print(movie)
+
+# movie.apend(input("enter 1st movie"))
+
+
+# palindrome list
+
+# list = ['m','a','a','m']
+# temp = list.copy()
+# temp.reverse()
+# if(list == temp):
+#     print("Palindrome")
+# else:
+#     print("not palindrome")
+
+# count grade
+
+grade = ('c','b','a','a','b','d')
+print(grade.count('a'))
+
+grade1 = ['c','b','a','a','b','d']
+grade1.sort()
+print(grade1)
