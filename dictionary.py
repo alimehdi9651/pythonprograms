b7fb5fcdd6b8e56fe1d34e56bec88cbce846d414
alimehdi <alimehdi9651@gmail.com>
2024-04-22 13:25:03 +0530

    Dictionary in python

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+# dictionary are mutable ,unordered and do not allow duplicate keys
+info = {
+    "college name" : "AIMT",
+    "name" : "ali",
+    "age" : 22
+}
+# print(info)
+# print(info["college name"])
+info["name"] = "Mehdi"
+info["is_adult"] = True
+print(info)
+print(info["age"])
