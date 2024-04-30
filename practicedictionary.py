aba3d7371912128c818ce32a73dd1c2d181910ea
alimehdi <alimehdi9651@gmail.com>
2024-04-30 13:17:00 +0530

    while loop

--- practicedictionary.py
+++ practicedictionary.py
@@ -1,5 +1,26 @@
-dict = {
-    "table" : ["a piece of furniture","list of facts and figure"],
-    "cat" : "a small animal" 
-}
-print(dict)
+# 1.
+
+# dict = {
+#     "table" : ["a piece of furniture","list of facts and figure"],
+#     "cat" : "a small animal" 
+# }
+# print(dict)
+
+# 2.
+
+# student =  {"java", "python","c", "c++", "c++", "python", "java"}
+# print("clas needed :",len(students))
+
+stud = {}
+
+# print(type(stud))
+# subject = input("enter first subject : ")
+# marks = int(input("enter marks of: "))
+# stud.update({subject : marks})
+# subject = input("enter second subject : ")
+# marks = int(input("enter marks of: "))
+# stud.update({subject : marks})
+# subject = input("enter third subject : ")
+# marks = int(input("enter marks of: "))
+# stud.update({subject : marks})
+# print(stud)
