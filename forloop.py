2b23b67dd4db396f24344aa0215d3f5812a1cf7d
alimehdi <alimehdi9651@gmail.com>
2024-05-01 14:59:35 +0530

    for loop

---
+++ forloop.py
@@ -0,0 +1,27 @@
+# list = [1, 2, 3, 3, 4]
+# for num in list:
+# #     print(num)
+
+
+# str = "ali mehdi"
+# for ch in str:
+#     if(ch == 'e'):
+#         print("e is present in the string")
+#         break
+#     print(ch)
+# else:
+#     print("end")
+
+
+# list = [1, 4, 8, 9, 16 , 25, 36]
+# for el in list:
+#     print(el)
+
+
+tup = (1, 4, 9, 25, 36, 49, 64)
+x = 49
+for el in tup:
+    if(el == x):
+        print(x, "is present in the list")
+    else:
+        print("finding")
