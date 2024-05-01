4422243d52a3b7993b70dec8bcbbeed5dae740f0
alimehdi <alimehdi9651@gmail.com>
2024-05-01 16:10:11 +0530

    practice question and range

--- forloop.py
+++ forloop.py
@@ -18,10 +18,53 @@
 #     print(el)
 
 
-tup = (1, 4, 9, 25, 36, 49, 64)
-x = 49
-for el in tup:
-    if(el == x):
-        print(x, "is present in the list")
-    else:
-        print("finding")
+# tup = (1, 4, 9, 25, 36, 49, 64)
+# x = 49
+# for el in tup:
+#     if(el == x):
+#         print(x, "is present in the list")
+#     else:
+#         print("finding")
+
+
+# range() this function returns the sequence of number that starts for 0 and 
+# increment by 1 by default and stops before a specified number
+# for i in range(5):
+#     print(i)
+
+
+# for i in range(10): # range(stop)
+#     print(i)
+
+
+# for i in range(2,10): # range(start, stop)
+#     print(i)
+
+
+# for i in range(2,10,2): # range(start, stop, steps)
+#     print(i)
+
+
+# print all even number for 2 to 100
+
+# for i in range(2,101,2):
+#     print(i)
+
+# print all odd number for 1 to 100
+# table of four
+# for i in range(4,41,4):
+#     print(i)
+
+
+
+# print number from 100 to 1
+# for i in range(100, 0, -1):
+#     print(i)
+
+
+
+# table of n
+# n = int(input("give number :"))
+# for i in range(1,11):
+#     print(i*n)
+
---
+++ practiceloops.py
@@ -0,0 +1,15 @@
+# print sum of n numbers
+
+# n = int(input("give the value of n: "))
+# sum = 0
+# while(n>0):
+#     sum += n
+#     n -= 1
+# print(sum)
+n = 4
+fact = 1
+# pritn factorial of a number n
+for i in range(1,n+1):
+    fact = fact * n
+
+print(fact)
