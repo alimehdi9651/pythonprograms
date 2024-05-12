a3f4118f5f2c0a1c44e1b8299818c947d5a8795e
alimehdi <alimehdi9651@gmail.com>
2024-05-12 14:34:32 +0530

    Updated modes

--- file.py
+++ file.py
@@ -1,8 +1,34 @@
-f = open("text.txt", "r")
-data = f.read()
-print(data)
+#read mode i.e "r"
+
+# f = open("text.txt", "r")
+# data = f.read()
+# # print(data)
 # line1 = f.readline()
 # print(line1)
 # line2 = f.readline()
 # print(line2)
-f.close()
+
+
+# write mode i.e "w"(overwrite) and "a"(append)
+# f = open("text.txt", "w")
+# f.write("hi i am mehdi")
+# f =  open("text.txt", "a")
+# f.write("\nand i am ali")
+# f.close()
+
+
+# if any txt file does not exixt in our folder then these mode automically create a file
+
+# f = open("sample.txt", "a")
+
+# "r+" mode read and overwrite(pointer start) -> no truncate
+#  "w+" mode read and overwrite(poinetr start) -> truncate
+#  "a+" mode read and overwrite (pointer end) -> no truncate
+
+# with open("text.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+# with automatically close the file
+    
+with open("text.txt", "w") as f:
+    f.write("hi")
