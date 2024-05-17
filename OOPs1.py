e50d7c45ae319ec949ed1854215516e1bf18547a
alimehdi <alimehdi9651@gmail.com>
2024-05-17 12:37:47 +0530

    inheritance

--- OOPs1.py
+++ OOPs1.py
@@ -1,4 +1,4 @@
-# Abstraction -> Hiding the implementation details of the class
+# ABSTRACTION -> Hiding the implementation details of the class
 # #  and only showing thefeatures to the user
 # class Car:
 #     def __init__(self):
@@ -15,34 +15,76 @@
 # car1.start_car()
 
 
-# Encapsulation -> wrapping data and function into a single unit (object)
+# ENCAPSULATION-> wrapping data and function into a single unit (object)
 #everything which we studied till now is an example of encapsulation 
 
 
 #Create account class with 2 attri balance and account number
 #and WAF that debit and credit ammount                                          
-class Account:
-    def __init__(self, blnc, acc_no):
-        self.balance = blnc
-        self.account_no = acc_no
+# class Account:
+#     def __init__(self, blnc, acc_no):
+#         self.balance = blnc
+#         self.account_no = acc_no
 
-    def debit(self,amt):
-        self.balance -= amt
-        print("Rs.", amt ,"is debited")
-        print("total balance = ", self.balance)
-    def credit(self,amt):
-        self.balance += amt
-        print("Rs.", amt, "is credited")
-        print("total balance =", self.balance)
+#     def debit(self,amt):
+#         self.balance -= amt
+#         print("Rs.", amt ,"is debited")
+#         print("total balance = ", self.balance)
+#     def credit(self,amt):
+#         self.balance += amt
+#         print("Rs.", amt, "is credited")
+#         print("total balance =", self.balance)
 
-    def print_baalance(self):
-        return self.balance
+#     def print_baalance(self):
+#         return self.balance
     
 
-acc1 = Account(10000, 12345)
-acc1.debit(200)
+# acc1 = Account(10000, 12345)
+# acc1.debit(200)
 
-acc1.credit(500)
-acc1.credit(50000)
-acc1.debit(45000)
-print("Yout current balance =",acc1.print_baalance())
+# acc1.credit(500)
+# acc1.credit(50000)
+# acc1.debit(45000)
+# print("Yout current balance =",acc1.print_baalance())
+
+
+
+# del keyword -> which is use to delete any object or attribute of an object
+# private attribute -> to make any attribite private we have to simply apply two 
+# underscore before the name of any attribute
+# class Student:
+#     def __init__(self, name, password):
+#         self.name = name
+#         self.__password = password #here __password is a private attribute of student class.
+# #this private attribute only lies in class
+#     def print_pass(self):
+#         print(self.__password)
+# s1 = Student("ali","manali24")
+
+# # del s1.name  
+# print(s1)
+# s1.print_pass()
+# print(s1.__password)
+
+# INHERITANCE -> when one class drives the properties(attributes) and method of another class.
+
+class Car():
+    colour = "black"
+    @staticmethod
+    def start():
+        print("car started")
+    @staticmethod
+    def stop():
+        print("Car stoped")
+    
+class ToyotaCars(Car):
+    def __init__(self, name):
+        self.name = name
+
+    def print_name(self):
+        print (self.name)
+
+car1 = ToyotaCars("Fortuner")
+print(car1.name)
+car1.start()
+print(car1.colour)
