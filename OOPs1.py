d0d9a9e559ebbd411fb269fdf30f7ae580dd59a6
alimehdi <alimehdi9651@gmail.com>
2024-05-21 16:22:26 +0530

    property decorator

--- OOPs1.py
+++ OOPs1.py
@@ -68,50 +68,87 @@
 
 # INHERITANCE -> when one class drives the properties(attributes) and method of another class.
 
-class Car():
-    def __init__(self, type):
-        self.type = type
-    @staticmethod
-    def start():
-        print("car started")
-    @staticmethod
-    def stop():
-        print("Car stoped")
-
-# Inheritance are to three types:
-#1 Single level inheritance
-#2 multi level inheritance 
-#3 Multiple inheritance
-    
-class ToyotaCars(Car):  # this is a example of single inheritance
-
-    def __init__(self, name, type):
-        super().__init__(type)
-        self.name= name
-
-# class KIA():
-#     def __init__(self, name):
-#         self.name = name
-
-class Fortuner(ToyotaCars):# this is the example of multi inheritance.
-
-    def __init__(self, milage, type):
-        super().__init__(type)
-        self.milage = milage
-        super().start()
-car1 = Fortuner(9, "petrol")
-print(car1.type)
-car1.start()
-# car1 = Fortuner("prtrol")
-# print(car1.type)
-# # car1.start()
-
-# class Saltos(Car , KIA): # this is example of multiple inheritance
+# class Car():
 #     def __init__(self, type):
 #         self.type = type
+#     @staticmethod
+#     def start():
+#         print("car started")
+#     @staticmethod
+#     def stop():
+#         print("Car stoped")
 
-# car2 = Saltos("Eletric")
+# # Inheritance are to three types:
+# #1 Single level inheritance
+# #2 multi level inheritance 
+# #3 Multiple inheritance
+    
+# class ToyotaCars(Car):  # this is a example of single inheritance
+
+#     def __init__(self, name, type):
+#         super().__init__(type)
+#         self.name= name
+
+# # class KIA():
+# #     def __init__(self, name):
+# #         self.name = name
+
+# class Fortuner(ToyotaCars):# this is the example of multi inheritance.
+
+#     def __init__(self, milage, type):
+#         super().__init__(type)
+#         self.milage = milage
+#         super().start()
+# car1 = ToyotaCars(9, "petrol")
+# print(car1.type)
+# car1.start()
+# # car1 = Fortuner("prtrol")
+# # print(car1.type)
+# # # car1.start()
+
+# # class Saltos(Car , KIA): # this is example of multiple inheritance
+# #     def __init__(self, type):
+# #         self.type = type
+
+# # car2 = Saltos("Eletric")
 
 
 
-# super() -> use to access methods of parent class.
+# # super() -> use to access methods of parent class.
+
+# class method -> basically this method is use to change class attribute.
+# class Person:
+#     name = "abc"
+
+#     @classmethod
+#     def changeName(cls, name):# here cls refers to class
+#         cls.name = name
+
+# p1 = Person()
+# p1.changeName("ali")
+# print(p1.name)
+# print(Person.name)
+
+
+
+
+# when we cannot give a fixed value to an attribute then we have to use "property decorator"
+# propert decorator -> we use property decorator on any method in a class to use to 
+# use the method a property/attribute
+
+class Student:
+    def __init__(self, phy, chem, bio):
+        self.phy = phy
+        self.chem = chem 
+        self.bio = bio
+        # self.percentage = (self.phy + self.chem + self.bio) / 3
+    @property
+    def percentage(self):# here this method precentage works as attribute 
+        return str((self.phy + self.chem + self.bio) / 3) + "%"
+s1 = Student(56, 57, 51)
+s1.phy = 99
+s1.chem = 97
+s1.bio = 98
+print(s1.phy)
+print(s1.percentage)#here percentage value will not reflect hence for
+# this percentage attribute we have to property decorator
