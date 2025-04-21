d9bab762628bb75e7433566cf4e8843aa1de8172
alimehdi <alimehdi9651@gmail.com>
2025-04-21 16:54:02 +0530

    Manupilation

--- manuplite_rows_and_columns.py
+++ manuplite_rows_and_columns.py
@@ -78,10 +78,10 @@ df = pd.DataFrame(data)
 #using isnull() function
 
 data2 = {
-    "Name": ["Ali", None, "Sara", "John", "Ayesha", "Ravi", "Neha", "Zaid", "Priya", "Rahul"],
-    "Salary": [50000, None, 47000, 60000, 53000, 52000, 48000, 58000, 49000, 51000],
-    "Age": [25, None, 24, 30, 27, 28, 23, 29, 26, 25],
-    "Performance": [88, None, 76, 98, 85, 82, 74, 91, 79, 80]
+    "Name": ["Ali", 'Mehdi', "Sara", "John", "Ayesha", "Ravi", "Neha", "Zaid", "Priya", "Rahul"],
+    "Salary": [50000, 52000, 47000, 60000, 53000, 52000, 48000, 58000, 49000, 51000],
+    "Age": [23, 24, 24, 30, 27, 30, 23, 30, 26, 26],
+    "Performance": [88, 80, 76, 98, 85, 82, 74, 91, 79, 80]
 }
 df2 = pd.DataFrame(data2)
 # print(df2.isnull())
@@ -93,8 +93,69 @@ print(df2)
 
 #if those missing are not useful we can simply drop or delete that none data
 #usning dropna() function
-df2.dropna(inplace=True)
-print(df2)
+# df2.dropna(inplace=True)
+# print(df2)
 
 #using this dropna() we can simply delete the None Row
 # or None data
+
+
+#if you want to fill some data at None or NaN then we use fillna(value , inplace = true)
+#here value will bw replace with None or NaN
+
+# df2.fillna(0, inplace=True) 
+# #here none and nan gonna fill with 0, we can use this if we want to fill some default value in it.
+# print(df2)
+
+
+# if we want to fill some useful data at none then we have to target a particular row
+#we have two options 
+# df2['Age'].fillna(df2["Age"].mean(), inplace=True)
+
+# df2["Age"] = df2['Age'].fillna(df2["Age"].mean()) # preffered one
+# df2["Salary"] = df2['Salary'].fillna(df2["Salary"].mean())
+# print(df2)
+
+
+
+
+
+#Interpolation : if we want to get some meaning full data at the place on None 
+# then we use interpolation, it put some mathamatically calculated value, based on previous data present in the series, at the place of None 
+#using interpolate(mehtod="", axis = 0), inplace = true: here method can be linear, polynomial or time 
+# df2["Salary"] = df2["Salary"].interpolate(method="linear")
+# # print(df2)
+# dates = pd.date_range(start='2025-01-01', periods=6, freq='D')
+# print(dates)
+
+
+
+
+#Aggregation and sorting
+# df2.sort_values(by = "Salary", ascending=True, inplace=True)
+# print(df2)
+
+
+#if you want to sort multiple rows
+# then pass list in by and assending arguments of sort_values
+# df2.sort_values(by = ["Age", "Salary"], ascending=[True, True], inplace=True)
+# print(df2)
+
+
+
+#Aggregation refers to the specific calculation throughout the colum
+#it can be mean() sum() avg() and many more
+# avg_salary = df2["Salary"].mean()
+# avg_salary = df2["Salary"].sum()
+# print(avg_salary)
+
+
+
+#Grouping in pandas
+#we can group similar data for future aggrigation, where grouing help us to find the sum(), count(), mean(),  min() , std()
+# and many more such operation on those similar data or values present in dataFrame
+# group = df2.groupby("Age")["Salary"].max()
+# print(group)
+#We can group multiple colum data just by adding colum name
+groups = df2.groupby(["Age", "Name"])["Salary"].max()
+print(groups)
